ea2c3c9e06d321e9154d4918076e4aa014159f0a
LucaRitz <ritz.luca@gmail.com>
2022-04-12 08:47:50 +0200

    Removed app, added usage for cmake

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This library implements the laplacian-pyramid concept described in the
 paper "The Laplacian Pyramid as a Compact Image Code".
 
-For usage have a look at [main.cpp](app/src/main.cpp) or on the below code snippet.
+For usage have a look at [laplacian_pyramid_test.cpp](test/laplacian-pyramid/src/laplacian_pyramid_test.cpp) or on the below code snippet.
 
 ```cpp
 
@@ -11,19 +11,16 @@ For usage have a look at [main.cpp](app/src/main.cpp) or on the below code snipp
 int main(int argc, char* argv[]) {
 
     cv::Mat image = cv::imread("resources/lena.png", cv::IMREAD_GRAYSCALE);
-    {
-        cv::imshow("Original", image);
-    }
+    cv::imshow("Original", image);
     
     image.convertTo(image, CV_32F);
-    auto pyramid = laplacian::LaplacianPyramid{image, 5};
     
-    {
-        auto decoded = pyramid.decode();
-        decoded.convertTo(decoded, CV_8U);
-        cv::imshow("Decoded", decoded);
-        cv::waitKey(0);
-    }
+    auto pyramid = laplacian::LaplacianPyramid{image, 5}; // Pyramid creation
+    auto decoded = pyramid.decode(); // Decoding into cv::Mat
+    
+    decoded.convertTo(decoded, CV_8U);
+    cv::imshow("Decoded", decoded);
+    cv::waitKey(0);
 
     return 0;
 }
@@ -33,3 +30,16 @@ int main(int argc, char* argv[]) {
 The library is built as a shared-lib with a minimum required CMake-Version of 3.15.1.<br>
 Tested platforms:
 - Windows 10 / MSVC 14.29.30133
+
+## Recommended usage in cmake:
+```cmake
+ExternalProject_add(
+    GIT_REPOSITORY https://github.com/LucaRitz/laplacian-pyramid
+    GIT_TAG        main
+    CMAKE_ARGS
+    -DBUILD_TEST=OFF
+    -DBUILD_DOCUMENTATION=OFF
+    -DBUILD_CODE_DOCUMENTATION=OFF
+    -DBUILD_EXT_LIBS=OFF
+)
+```
--- test/laplacian-pyramid/src/laplacian_pyramid_test.cpp
+++ test/laplacian-pyramid/src/laplacian_pyramid_test.cpp
@@ -1,6 +1,52 @@
 #include <gtest/gtest.h>
 #include <laplacian-pyramid/laplacian_pyramid.hpp>
-#include <fstream>
-#include <nlohmann/json.hpp>
-#include <iostream>
 #include <chrono>
+#include <functional>
+#include <string>
+
+namespace laplacian::test {
+
+    template<class R>
+    R measured(const std::function<R ()>& function, const std::string& step = "");
+}
+
+TEST(LaplacianPyramidTest, should_display_decoded_image_if_image_is_grayscale) {
+
+    cv::Mat image = cv::imread("resources/lena.png", cv::IMREAD_GRAYSCALE);
+    cv::imshow("Original", image);
+
+    image.convertTo(image, CV_32F);
+
+    auto pyramid = laplacian::test::measured<laplacian::LaplacianPyramid>(
+            [&image]() -> laplacian::LaplacianPyramid { return laplacian::LaplacianPyramid{image, 5}; },
+            "Laplace-Pyramid creation");
+
+    auto decoded = laplacian::test::measured<cv::Mat>(
+            [&pyramid]() -> cv::Mat { return pyramid.decode(); }, "Laplace-Pyramid decode");
+    decoded.convertTo(decoded, CV_8U);
+    cv::imshow("Decoded", decoded);
+    cv::waitKey(0);
+}
+
+TEST(LaplacianPyramid, should_display_decoded_image_if_image_is_color) {
+
+    // TODO: Implement
+}
+
+TEST(LaplacianPyramid, shold_display_decoded_image_if_image_is_quantized) {
+
+    // TODO: Implement
+}
+
+template<class R>
+R laplacian::test::measured(const std::function<R ()>& function, const std::string& step) {
+
+    auto start = std::chrono::high_resolution_clock::now();
+    auto result = function();
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+    std::cout << (step.empty() ? "Measured step" : step) << " took " << std::to_string(duration.count()) << " ms" << std::endl;
+
+    return result;
+}
